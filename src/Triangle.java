2dde6afdf3bb87cade6b634f14812e6567cdfa6d
raphaelhudvoy <raphael.hudon-voyer@mail.mcgill.ca>
2014-02-23 13:26:52 -0500

    test 1 to 6

--- src/Triangle.java
+++ src/Triangle.java
@@ -1,11 +1,50 @@
 
 public class Triangle {
+	
+	// Validation messages
+	public static final String INSUFFICIENT_SIDES = "Insufficient number of sides were provided";
+	public static final String TO_MANY_SIDES = "To many sides";
+	public static final String SUCCESS	= "No problem occured";
+	public static final String WRONG_INPUT_TYPE = "Inputs should be integers";
 
+	
+	private int A, B, C;
+	
+	public Triangle () {
+		
+	}
+	
+	public String setSides (String args[]) {
+		
+		if (args.length < 3) {
+			return INSUFFICIENT_SIDES;
+		} if (args.length > 3){
+			return TO_MANY_SIDES;
+		} else {
+			
+			try {
+				this.A = Integer.parseInt(args[0]);
+				this.B = Integer.parseInt(args[1]);
+				this.C = Integer.parseInt(args[2]);
+				
+				return SUCCESS;
+				
+			} catch (Exception e) {
+				return WRONG_INPUT_TYPE;
+			}
+			
+			
+		}
+				
+	}
+	
+	
+	
 	/**
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		//
 
 	}
 
--- tests/TriangleTests.java
+++ tests/TriangleTests.java
@@ -4,10 +4,82 @@ import org.junit.Test;
 
 
 public class TriangleTests {
+	
+	@Test
+	public void wrongInputDouble() {
+		
+		// Initialization
+		Triangle triangle = new Triangle();
+		String[] sides = {"4.4","4.9", "4.0"};
+		
+		// Test functions
+		String res = triangle.setSides(sides);
+		
+		//Assert
+		assertEquals(res, Triangle.WRONG_INPUT_TYPE);
+	
+	}
+	
+	@Test
+	public void wrongInputString() {
+		
+		// Initialization
+		Triangle triangle = new Triangle();
+		String[] sides = {"a","4", "4"};
+		
+		// Test functions
+		String res = triangle.setSides(sides);
+		
+		//Assert
+		assertEquals(res, Triangle.WRONG_INPUT_TYPE);
+	
+	}
+	
+	@Test
+	public void threeSides() {
+		
+		// Initialization
+		Triangle triangle = new Triangle();
+		String[] sides = {"3","2", "4"};
+		
+		// Test functions
+		String res = triangle.setSides(sides);
+		
+		//Assert
+		assertEquals(res, Triangle.SUCCESS);
+	
+	}
+	
+	@Test
+	public void ToManySides() {
+		
+		// Initialization
+		Triangle triangle = new Triangle();
+		String[] sides = {"3","2", "4", "6"};
+		
+		// Test functions
+		String res = triangle.setSides(sides);
+		
+		//Assert
+		assertEquals(res, Triangle.TO_MANY_SIDES);
+	
+	}
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void notEnoughSides() {
+		
+		// Initialization
+		Triangle triangle = new Triangle();
+		String[] sides = {"3","2"};
+		
+		// Test functions
+		String res = triangle.setSides(sides);
+		
+		//Assert
+		assertEquals(res, Triangle.INSUFFICIENT_SIDES);
+	
 	}
+	
+	
 
 }
