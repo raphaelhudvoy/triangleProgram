3578f6a29e894a54616bd4132a8675a96ba51e84
raphaelhudvoy <raphael.hudon-voyer@mail.mcgill.ca>
2014-02-23 15:48:18 -0500

    all tests

--- src/Triangle.java
+++ src/Triangle.java
@@ -6,28 +6,117 @@ public class Triangle {
 	public static final String TO_MANY_SIDES = "To many sides";
 	public static final String SUCCESS	= "No problem occured";
 	public static final String WRONG_INPUT_TYPE = "Inputs should be integers";
+	public static final String INTEGER_OUT_OF_BOUNDS = "Inputs are too small or too big";
+	public static final String NOT_A_TRIANGLE = "Not a triangle";
+	public static final String SCALENE = "Scalene triangle";
+	public static final String ISOSCELE = "Isocele triangle";
+	public static final String EQUILATERAL = "Equilateral triangle";
+
+
 
 	
 	private int A, B, C;
 	
-	public Triangle () {
+	private boolean checkSides (int a, int b, int c) throws Exception{
 		
+		if ( a + b < c) {
+			throw new Exception(NOT_A_TRIANGLE);
+		} else {
+			return true;
+		}		
+	}
+	
+	
+	
+	private boolean isScalene () {
+		if (A != B && A != C && C != B) {
+			return true;
+		} else {
+			return false;
+		}
+	}
+	
+
+	private boolean isIsoscele () {
+		
+		if (A == B && A == C && B == C) { // make sure is not equilateral
+			return false;
+		} else {
+			if (A == B || A == C || B == C) {
+				return true;
+			} else {
+				return false;
+			}
+		}
+	}
+	
+	private boolean isEquilateral () {
+		
+		if (A == B && A == C && B == C) {
+			return true;
+		} else {
+			return false;
+		}
+		
+	}
+	
+	public String processTriangle () {
+		
+		try {
+			checkSides(A, B, C);	// A + B >= C	
+			checkSides(A, C, B);	// A + B >= C		
+			checkSides(B, C, A);	// A + B >= C			
+			
+			if (isScalene()) {
+				return SCALENE;
+			} else if (isIsoscele()) {
+				return ISOSCELE;
+			} else if (isEquilateral()) {
+				return EQUILATERAL;
+			} else {
+				return NOT_A_TRIANGLE;
+			}
+		
+		} catch (Exception e) {
+			return NOT_A_TRIANGLE;
+		}
+	}
+	
+	private int checkBounds (int number) throws Exception {
+		
+		
+		if (number < 0 || number > 100) {
+			throw new Exception(INTEGER_OUT_OF_BOUNDS);
+		} else {
+			return number;
+		}
 	}
 	
 	public String setSides (String args[]) {
 		
 		if (args.length < 3) {
 			return INSUFFICIENT_SIDES;
+			
 		} if (args.length > 3){
 			return TO_MANY_SIDES;
+			
 		} else {
 			
 			try {
-				this.A = Integer.parseInt(args[0]);
-				this.B = Integer.parseInt(args[1]);
-				this.C = Integer.parseInt(args[2]);
+				int a = Integer.parseInt(args[0]);
+				int b = Integer.parseInt(args[1]);
+				int c = Integer.parseInt(args[2]);
 				
-				return SUCCESS;
+				try {
+					this.A = checkBounds(a);
+					this.B = checkBounds(b);
+					this.C = checkBounds(c);
+					
+					return SUCCESS;
+					
+				} catch (Exception e) {
+					return INTEGER_OUT_OF_BOUNDS;
+				}
 				
 			} catch (Exception e) {
 				return WRONG_INPUT_TYPE;
@@ -44,7 +133,18 @@ public class Triangle {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		//
+		
+		Triangle tri = new Triangle();
+		
+		String res = tri.setSides(args);
+		
+		if (res == SUCCESS) {
+			String result = tri.processTriangle();
+			System.out.println(result);
+			
+		} else {
+			System.out.println(res);
+		}
 
 	}
 
--- tests/TriangleTests.java
+++ tests/TriangleTests.java
@@ -6,6 +6,208 @@ import org.junit.Test;
 public class TriangleTests {
 	
 	@Test
+	public void SidesAllEqual() {
+		
+		// Initialization
+		Triangle triangle = new Triangle();
+		String[] sides = {"11","11","11"};
+		
+		// Test functions
+		String res = triangle.setSides(sides);
+		assertEquals(res, Triangle.SUCCESS);
+		
+		res = triangle.processTriangle();
+		
+		//Assert
+		System.out.println(res);
+		assertEquals(res, Triangle.EQUILATERAL);
+	
+	}
+	
+	@Test
+	public void isosceleSidesBandCequal() {
+		
+		// Initialization
+		Triangle triangle = new Triangle();
+		String[] sides = {"13","11","11"};
+		
+		// Test functions
+		String res = triangle.setSides(sides);
+		assertEquals(res, Triangle.SUCCESS);
+		
+		res = triangle.processTriangle();
+		
+		//Assert
+		System.out.println(res);
+		assertEquals(res, Triangle.ISOSCELE);
+	
+	}
+	
+	@Test
+	public void isosceleSidesAandCequal() {
+		
+		// Initialization
+		Triangle triangle = new Triangle();
+		String[] sides = {"11","13","11"};
+		
+		// Test functions
+		String res = triangle.setSides(sides);
+		assertEquals(res, Triangle.SUCCESS);
+		
+		res = triangle.processTriangle();
+		
+		//Assert
+		System.out.println(res);
+		assertEquals(res, Triangle.ISOSCELE);
+	
+	}
+	
+	@Test
+	public void IsoscelesSidesAandBequal() {
+		
+		// Initialization
+		Triangle triangle = new Triangle();
+		String[] sides = {"12","12","11"};
+		
+		// Test functions
+		String res = triangle.setSides(sides);
+		assertEquals(res, Triangle.SUCCESS);
+		
+		res = triangle.processTriangle();
+		
+		//Assert
+		System.out.println(res);
+		assertEquals(res, Triangle.ISOSCELE);
+	
+	}
+	
+	@Test
+	public void ScaleneTriangle() {
+		
+		// Initialization
+		Triangle triangle = new Triangle();
+		String[] sides = {"12","6","14"};
+		
+		// Test functions
+		String res = triangle.setSides(sides);
+		assertEquals(res, Triangle.SUCCESS);
+		
+		res = triangle.processTriangle();
+		
+		//Assert
+		System.out.println(res);
+		assertEquals(res, Triangle.SCALENE);
+	
+	}
+	
+	
+	
+	@Test
+	public void SumOfBandCSmallerThenA() {
+		
+		// Initialization
+		Triangle triangle = new Triangle();
+		String[] sides = {"7","3","3"};
+		
+		// Test functions
+		String res = triangle.setSides(sides);
+		assertEquals(res, Triangle.SUCCESS);
+		
+		res = triangle.processTriangle();
+		
+		//Assert
+		System.out.println(res);
+		assertEquals(res, Triangle.NOT_A_TRIANGLE);
+	
+	}
+	
+	@Test
+	public void SumOfAandCSmallerThenB() {
+		
+		// Initialization
+		Triangle triangle = new Triangle();
+		String[] sides = {"3","7", "3"};
+		
+		// Test functions
+		String res = triangle.setSides(sides);
+		assertEquals(res, Triangle.SUCCESS);
+		
+		res = triangle.processTriangle();
+		
+		//Assert
+		System.out.println(res);
+		assertEquals(res, Triangle.NOT_A_TRIANGLE);
+	
+	}
+	
+	@Test
+	public void SumOfAandBSmallerThenC() {
+		
+		// Initialization
+		Triangle triangle = new Triangle();
+		String[] sides = {"4","4", "9"};
+		
+		// Test functions
+		String res = triangle.setSides(sides);
+		assertEquals(res, Triangle.SUCCESS);
+		
+		res = triangle.processTriangle();
+		
+		//Assert
+		System.out.println(res);
+		assertEquals(res, Triangle.NOT_A_TRIANGLE);
+	
+	}
+	
+	
+	
+	@Test
+	public void Inputbetween1and100InvalidtooBig() {
+		
+		// Initialization
+		Triangle triangle = new Triangle();
+		String[] sides = {"4","101", "50"};
+		
+		// Test functions
+		String res = triangle.setSides(sides);
+		
+		//Assert
+		assertEquals(res, Triangle.INTEGER_OUT_OF_BOUNDS);
+	
+	}
+	
+	
+	@Test
+	public void Inputbetween1and100InvalidtooSmall() {
+		
+		// Initialization
+		Triangle triangle = new Triangle();
+		String[] sides = {"-3","100", "50"};
+		
+		// Test functions
+		String res = triangle.setSides(sides);
+		
+		//Assert
+		assertEquals(res, Triangle.INTEGER_OUT_OF_BOUNDS);
+	
+	}
+		
+	@Test
+	public void Inputbetween1and100Valid() {
+		
+		// Initialization
+		Triangle triangle = new Triangle();
+		String[] sides = {"1","100", "50"};
+		
+		// Test functions
+		String res = triangle.setSides(sides);
+		
+		//Assert
+		assertEquals(res, Triangle.SUCCESS);
+	
+	}
+	
+	@Test
 	public void wrongInputDouble() {
 		
 		// Initialization
